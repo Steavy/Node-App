bc9dbde226dd0cdd6b68cd044ae88a1fa177c2da
Steven Nienhuis <steven@qualityaccelerators.nl>
2023-02-20 16:22:59 +0100

    fix test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World New Docker Image broken for QA!';
+    return 'Hello World New Docker Image!';
   }
 }
